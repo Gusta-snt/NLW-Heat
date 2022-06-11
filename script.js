e848d61c4ca57cff0b68ea599f0557e22f111af6
Gustavo Ferreira <gustavofrsnt@gmail.com>
2022-06-11 15:26:47 -0300

    Stage 4
    Tornando o projeto mais dinâmico.

--- script.js
+++ script.js
@@ -7,13 +7,34 @@ const userSocialMedia = {
 }
 
 function changeSociaMediaLinks(links){
-    userName.textContent = "Gustavo Ferreira";
-    githubLink.href = `https://github.com/${userSocialMedia.github}`;
+    //userName.textContent = "Gustavo Ferreira";
+    //githubLink.href = `https://github.com/${userSocialMedia.github}`;
 
     for(let li of socialLinks.children){
         const social = li.getAttribute("class");
-        li.children[0].href = `https://${social}.com/${userSocialMedia[social]}`
+        li.children[0].href = `https://${social}.com/${links[social]}`
     }
 }
 
 changeSociaMediaLinks(userSocialMedia);
+
+function getGitHubProfileInfos(userGithub) {
+
+    const url = `https://api.github.com/users/${userGithub.github}`;
+
+    fetch(url).then(response => response.json()).then(data => {
+        userName.textContent = data.name;
+        userLogin.textContent = data.login;
+        githubLink.href = data.html_url;
+        userImg.src = data.avatar_url;
+        userImg.alt = `Foto de ${data.name}`
+        Bio.textContent = data.bio;
+    });
+    
+}
+
+getGitHubProfileInfos(userSocialMedia);
+
+
+//arrow function
+
